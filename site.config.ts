3277cd091cd6c1fc7dc85fec6526a3fc9d523d53
Tecllast <78220282+Tecllast@users.noreply.github.com>
2022-11-12 12:51:01 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '8f1e258e5d7c409abe9ed37c2a1ffcf3',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Teclast' Blog',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Teclast',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Teclast' Personal Blog',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
